eb0ff2e02962ca314fb2fee702c7893702470a45
Vilson1984 <vcmorion@hotmail.com>
2023-09-13 11:02:00 -0300

    Load and comments created

--- ETL_empréstimo_cliente.py
+++ ETL_empréstimo_cliente.py
@@ -1,12 +1,21 @@
-"""1 - EXTRATION:
-Here we'll import csv library, build a archive extract """
+"""
+1 - EXTRACTION:
+Here we'll import csv to read and write the file builded at Excel
+The program will be Extract whole information tha file.csv 
+"""
+
 import csv
 
-arquivo = open("score_clientes.csv")
-scores = csv.reader(arquivo)
+arquivo = open("score_clientes.csv") 
+scores = csv.reader(arquivo) #create a new object scores to read the file
 
-"""2 - TRANSFORM"""
-table = list()
+"""
+2 - TRANSFORM:
+First we create a list that 'll be store whole modifies does at the table
+The Code, 'll check if score's client is high, medium or low, then 'll make a decision, 
+to lend money and especifing the lend's tax.
+"""
+table = list() #Create a list to store whole information
 
 for score in scores:
     
@@ -24,4 +33,16 @@ for score in scores:
         score.append("Empréstimo negado, consulte seu gerente.")
         table.append(score)
         print(score)
-  
+    
+    else:
+        score.append("decisao")
+        table.append(score)
+
+"""
+3 - LOAD:
+At last, will be create a new file, with the new decisions
+"""
+
+arquivo_novo = open('decisao.csv', 'w', newline='') #create a new archieve 'decisao.csv' which was created to write
+decisao = csv.writer(arquivo_novo, delimiter=',') #create a new object 'decisao' that write at 'decisao.csv'
+decisao.writerows(table) #Write all modifies at the table
